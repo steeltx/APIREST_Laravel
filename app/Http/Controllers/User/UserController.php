4046e0d9abf7502a3c45777ed4b117b030ed8456
steeltx <realsteel.gnu@gmail.com>
2021-02-28 01:28:08 -0600

    Se creo los metodos crud para usuario

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -14,17 +15,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $usuarios = User::all();
+        return response()->json(['data' => $usuarios], 200);
     }
 
     /**
@@ -35,7 +27,22 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Reglas de validacion
+        $validar = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed'
+        ]);
+
+        $campos = $request->all();
+        // Establecer los campos que el usuario no puede alterar
+        $campos['password'] = bcrypt($request->password);
+        $campos['verified'] = User::USUARIO_NO_VERIFICADO;
+        $campos['verification_token'] = User::generarVerificactionToken();
+        $campos['admin'] = User::USUARIO_REGULAR;
+
+        $usuario = User::create($campos);
+        return response()->json(['data' => $usuario], 201);
     }
 
     /**
@@ -46,18 +53,8 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        $usuario = User::findOrFail($id);
+        return response()->json(['data' => $usuario], 200);
     }
 
     /**
@@ -69,7 +66,54 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+        // Reglas de validacion
+        $validar = $request->validate([
+            'email' => 'email|unique:users,email,'.$user->id,
+            'password' => 'min:6|confirmed',
+            'admin' => 'in:' . User::USUARIO_ADMINISTRADOR . ',' . User::USUARIO_REGULAR
+        ]);
+
+        if ($request->has('name')) {
+            $user->name = $request->name;
+        }
+
+        if ($request->has('email') && $user->email != $request->email) {
+            $user->verified = User::USUARIO_NO_VERIFICADO;
+            $user->verification_token = User::generarVerificactionToken();
+            $user->email = $request->email;
+        }
+
+        if ($request->has('password')) {
+            $user->password = bcrypt($request->password);
+        }
+
+        if ($request->has('admin')) {
+            if (!$user->esVerificado()) {
+                return response()->json(
+                    [
+                        'error' =>  'Unicamente los usuarios verificados pueden cambiar su valor de administrador',
+                        'code' => 409
+                    ],
+                    409
+                );
+            }
+            $user->admin = $request->admin;
+        }
+
+        if(!$user->isDirty()){
+            return response()->json(
+                [
+                    'error' =>  'Se debe especificar al menos un valor diferente para actualizar',
+                    'code' => 422
+                ],
+                422
+            );
+        }
+
+        $user->save();
+
+        return response()->json(['data' => $user],200);
     }
 
     /**
@@ -80,6 +124,9 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->delete();
+        return response()->json(['data' => $user],200);
+
     }
 }
