f0f9a9950fe40008d6817954cfe7d6f36de55879
steeltx <realsteel.gnu@gmail.com>
2021-02-28 01:46:42 -0600

    Metodo index y show en SellerController

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Seller;
 
 use App\Http\Controllers\Controller;
+use App\Models\Seller;
 use Illuminate\Http\Request;
 
 class SellerController extends Controller
@@ -14,28 +15,9 @@ class SellerController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $vendedores = Seller::has('products')->get();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        return response()->json(['data' => $vendedores],200);
     }
 
     /**
@@ -46,40 +28,9 @@ class SellerController extends Controller
      */
     public function show($id)
     {
-        //
+        $vendedore = Seller::has('products')->findOrFail($id);
+
+        return response()->json(['data' => $vendedore],200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
