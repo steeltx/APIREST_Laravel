d95312edc3555dc651da6758aff3f947f218a8cf
steeltx <realsteel.gnu@gmail.com>
2021-03-06 23:26:28 -0600

    Operaciones de Seller

---
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Seller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class SellerProductController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Seller $seller)
+    {
+        $products = $seller->products;
+        return $this->showAll($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, User $seller)
+    {
+        $rules = [
+            'name' => 'required',
+            'description' => 'required',
+            'quantity' => 'required|integer|min:1',
+            'image' => 'required|image'
+        ];
+
+        $this->validate($request, $rules);
+
+        $data = $request->all();
+        $data['status'] = Product::PRODUCTO_NO_DISPONIBLE;
+        $data['image'] = '1.jpg';
+        $data['seller_id'] = $seller->id;
+
+        $product = Product::create($data);
+        return $this->showOne($product, 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Seller $seller, Product $product)
+    {
+        $rules = [
+            'quantity' => 'integer|min:1',
+            'status' => 'in:' . Product::PRODUCTO_DISPONIBLE . ',' . Product::PRODUCTO_NO_DISPONIBLE,
+            'image' => 'image'
+        ];
+
+        $this->validate($request, $rules);
+
+        $this->verificarVendedor($seller, $product);
+
+        $product->fill($request->only([
+            'name',
+            'descripction',
+            'quantity'
+        ]));
+
+        if($request->has('status')){
+            $product->status = $request->status;
+            if($product->estaDisponible() && $product->categories()->count() === 0){
+                return $this->errorResponse('Un producto debe tener al menos 1 categoria',409);
+            }
+        }
+
+        if($product->isClean()){
+            return $this->errorResponse('Se debe especificar al menos un valor diferente para actualizar',422);
+        }
+
+        $product->save();
+        return $this->showOne($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Seller  $seller
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Seller $seller, Product $product)
+    {
+        $this->verificarVendedor($seller, $product);
+        $product->delete();
+        return $this->showOne($product);
+    }
+
+    public function verificarVendedor(Seller $seller, Product $product){
+        if ($seller->id != $product->seller_id) {
+            throw new HttpException(422,'El vendedor no es el propietario');
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -21,34 +21,38 @@ use Illuminate\Support\Facades\Route;
 /**
  * Buyers
  */
-Route::resource('buyers','Buyer\BuyerController',['only' => ['index','show']]);
-Route::resource('buyers.transactions','Buyer\BuyerTransactionController',['only' => ['index']]);
-Route::resource('buyers.products','Buyer\BuyerProductController',['only' => ['index']]);
-Route::resource('buyers.sellers','Buyer\BuyerSellerController',['only' => ['index']]);
-Route::resource('buyers.categories','Buyer\BuyerCategoryController',['only' => ['index']]);
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => ['index']]);
+Route::resource('buyers.products', 'Buyer\BuyerProductController', ['only' => ['index']]);
+Route::resource('buyers.sellers', 'Buyer\BuyerSellerController', ['only' => ['index']]);
+Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' => ['index']]);
 /**
  * Categories
  */
-Route::resource('categories','Category\CategoryController',['except' => ['create','edit']]);
-Route::resource('categories.sellers','Category\CategorySellerController',['only' => ['index']]);
-Route::resource('categories.products','Category\CategoryProductController',['only' => ['index']]);
-Route::resource('categories.transactions','Category\CategoryTransactionController',['only' => ['index']]);
-Route::resource('categories.buyers','Category\CategoryBuyerController',['only' => ['index']]);
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+Route::resource('categories.sellers', 'Category\CategorySellerController', ['only' => ['index']]);
+Route::resource('categories.products', 'Category\CategoryProductController', ['only' => ['index']]);
+Route::resource('categories.transactions', 'Category\CategoryTransactionController', ['only' => ['index']]);
+Route::resource('categories.buyers', 'Category\CategoryBuyerController', ['only' => ['index']]);
 /**
  * Products
  */
-Route::resource('products','Product\ProductController',['only' => ['index','show']]);
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
 /**
  * Transactions
  */
-Route::resource('transactions','Transaction\TransactionController',['only' => ['index','show']]);
-Route::resource('transactions.categories','Transaction\TransactionCategoryController',['only' => ['index']]);
-Route::resource('transactions.sellers','Transaction\TransactionSellerController',['only' => ['index']]);
+Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+Route::resource('transactions.categories', 'Transaction\TransactionCategoryController', ['only' => ['index']]);
+Route::resource('transactions.sellers', 'Transaction\TransactionSellerController', ['only' => ['index']]);
 /**
  * Sellers
  */
-Route::resource('sellers','Seller\SellerController',['only' => ['index','show']]);
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' => ['index']]);
+Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only' => ['index']]);
+Route::resource('sellers.buyers', 'Seller\SellerBuyerController', ['only' => ['index']]);
+Route::resource('sellers.products', 'Seller\SellerProductController', ['except' => ['create', 'show', 'edit']]);
 /**
  * Users
  */
-Route::resource('users','User\UserController',['except' => ['create','edit']]);
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
